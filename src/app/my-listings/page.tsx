2a791fd4761d63fde4f80ccd4b7ddfb28f85b129
jtavis12345-dev <jtavis12345@gmail.com>
2026-02-09 16:48:29 -0700

    Add files via upload

---
+++ src/app/my-listings/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { MyListings } from "@/components/MyListings";
+
+export default function MyListingsPage() {
+  return (
+    <main className="w-full px-4 py-6 sm:px-6 lg:px-10">
+      <MyListings />
+    </main>
+  );
+}
